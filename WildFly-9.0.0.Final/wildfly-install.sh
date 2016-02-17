559590cef7e83d4e24bca67e67d579021b8fea93
WUMUXIAN <wumuxian1988@gmail.com>
2016-02-17 16:30:00 +0800

    try to create script that fire up a 2-nodes cluster

--- WildFly-9.0.0.Final/wildfly-install.sh
+++ WildFly-9.0.0.Final/wildfly-install.sh
@@ -12,9 +12,6 @@
 WILDFLY_VERSION='9.0.0.Final'
 WILDFLY_FILENAME="wildfly-$WILDFLY_VERSION"
 WILDFLY_ARCHIVE_NAME="$WILDFLY_FILENAME.tar.gz"
-WILDFLY_DOWNLOAD_ADDRESS="http://download.jboss.org/wildfly/$WILDFLY_VERSION/$WILDFLY_ARCHIVE_NAME"
-JDBC_ARCHIVE_NAME='postgresql-9.4-1206-jdbc42.jar'
-JDBC_DOWLOAD_ADDRESS="https://jdbc.postgresql.org/download/$JDBC_ARCHIVE_NAME"
 
 INSTALL_DIR=/opt
 WILDFLY_FULL_DIR=$INSTALL_DIR/$WILDFLY_FILENAME
@@ -22,7 +19,7 @@ WILDFLY_DIR=$INSTALL_DIR/wildfly
 
 WILDFLY_USER="wildfly"
 WILDFLY_SERVICE="wildfly"
-WILDFLY_MODE="standalone"
+WILDFLY_MODE="domain"
 
 WILDFLY_STARTUP_TIMEOUT=240
 WILDFLY_SHUTDOWN_TIMEOUT=30
@@ -33,35 +30,8 @@ echo "This script must be run as root."
 exit 1
 fi
 
-# Install Java
-echo "Install Java"
-sudo yum install wget -y
-wget --no-cookies --no-check-certificate "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm" --header "Cookie:gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;oraclelicense=accept-securebackup-cookie"
-sudo yum jdk-8u60-linux-x64.rpm -y
-sudo rm jdk-8u60-linux-x64.rpm
-export JAVA_HOME=/usr/java/jdk1.8.0_60/
-
-# Downlaoad wildfly
-echo "Downloading: $WILDFLY_DOWNLOAD_ADDRESS..."
-[ -e "$WILDFLY_ARCHIVE_NAME" ] && echo 'Wildfly archive already exists.'
-if [ ! -e "$WILDFLY_ARCHIVE_NAME" ]; then
-wget -q $WILDFLY_DOWNLOAD_ADDRESS
-if [ $? -ne 0 ]; then
-echo "Not possible to download Wildfly."
-exit 1
-fi
-fi
-
-# Download pg jdbc
-echo "Downloading: $JDBC_DOWLOAD_ADDRESS..."
-[ -e "$JDBC_ARCHIVE_NAME" ] && echo 'Postgres JDBC archive already exists.'
-if [ ! -e "$JDBC_ARCHIVE_NAME" ]; then
-wget -q $JDBC_DOWLOAD_ADDRESS
-if [ $? -ne 0 ]; then
-echo "Not possible to download postgresql JDBC."
-exit 1
-fi
-fi
+# copy wildfly and it's configurations
+cp /vagrant/$WILDFLY_ARCHIVE_NAME /home/vagrant/$WILDFLY_ARCHIVE_NAME
 
 echo "Cleaning up..."
 rm -rf "$WILDFLY_DIR"
@@ -82,26 +52,6 @@ chown -R $WILDFLY_USER:$WILDFLY_USER $WILDFLY_DIR/standalone/
 echo "Registrating Wildfly as service with launch and systemctl..."
 # if should use systemd
 if [ -x /bin/systemctl ]; then
-
-cat > $WILDFLY_DIR/bin/launch.sh << "EOF"
-#!/bin/sh
-if [ "x$WILDFLY_HOME" = "x" ]; then
-WILDFLY_HOME="/opt/wildfly"
-fi
-if [ "x$1" = "xdomain" ]; then
-echo 'Starting Wildfly in domain mode.'
-$WILDFLY_HOME/bin/domain.sh -c $2 -b $3
-#>> /var/log/$WILDFLY_SERVICE/server-`date +%Y-%m-%d`.log
-else
-echo 'Starting Wildfly in standalone mode.'
-$WILDFLY_HOME/bin/standalone.sh -c $2 -b $3
-#>> /var/log/$WILDFLY_SERVICE/server-`date +%Y-%m-%d`.log
-fi
-EOF
-
-# $WILDFLY_HOME is not visible here
-sed -i -e 's,WILDFLY_HOME=.*,WILDFLY_HOME='$WILDFLY_DIR',g' $WILDFLY_DIR/bin/launch.sh
-chmod +x $WILDFLY_DIR/bin/launch.sh
 
 cp $WILDFLY_DIR/bin/init.d/wildfly-init-redhat.sh /etc/init.d/$WILDFLY_SERVICE
 WILDFLY_SERVICE_CONF=/etc/default/wildfly.conf
@@ -124,15 +74,6 @@ echo JBOSS_HOME=\"$WILDFLY_DIR\" > $WILDFLY_SERVICE_CONF
 echo JBOSS_USER=$WILDFLY_USER >> $WILDFLY_SERVICE_CONF
 echo STARTUP_WAIT=$WILDFLY_STARTUP_TIMEOUT >> $WILDFLY_SERVICE_CONF
 echo SHUTDOWN_WAIT=$WILDFLY_SHUTDOWN_TIMEOUT >> $WILDFLY_SERVICE_CONF
-echo WILDFLY_CONFIG=$WILDFLY_MODE.xml >> $WILDFLY_SERVICE_CONF
-echo WILDFLY_MODE=$WILDFLY_MODE >> $WILDFLY_SERVICE_CONF
-echo WILDFLY_BIND=0.0.0.0 >> $WILDFLY_SERVICE_CONF
+echo JBOSS_DOMAIN_CONFIG=$WILDFLY_MODE.xml >> $WILDFLY_SERVICE_CONF
+echo JBOSS_MODE=$WILDFLY_MODE >> $WILDFLY_SERVICE_CONF
 fi
-
-echo "Configuring application server..."
-sed -i -e 's,<deployment-scanner path="deployments" relative-to="jboss.server.base.dir" scan-interval="5000",<deployment-scanner path="deployments" relative-to="jboss.server.base.dir" scan-interval="5000" deployment-timeout="'$WILDFLY_STARTUP_TIMEOUT'",g' $WILDFLY_DIR/$WILDFLY_MODE/configuration/$WILDFLY_MODE.xml
-sed -i -e 's,<inet-address value="${jboss.bind.address:127.0.0.1}"/>,<any-address/>,g' $WILDFLY_DIR/$WILDFLY_MODE/configuration/$WILDFLY_MODE.xml
-
-[ -x /bin/systemctl ] && systemctl start $WILDFLY_SERVICE || service $WILDFLY_SERVICE start
-
-echo "Done."
